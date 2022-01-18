42bb8276f516bde1b2b25185a0668fcc6ddc1807
sangwani <mightypz@gmail.com>
2022-01-18 21:59:35 +0200

    debug

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #Auth: Sangwani P Zyambo
-puts ARGV[0].scan(/\[from:(.*?\] \[to:.*?)\] \[flags:(.*?\]/).join(",")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
