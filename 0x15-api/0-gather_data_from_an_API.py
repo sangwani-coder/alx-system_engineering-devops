0a46633d26d1f29882ab6d6fd135b40fcdadc419
Sangwani <mightyp2@gmail.com>
2022-04-12 05:42:24 +0200

    fix first line foramtting

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     complete = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks ({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
           user.get("name"), len(complete), len(todos)))
     [print("\t {}".format(c)) for c in complete]
