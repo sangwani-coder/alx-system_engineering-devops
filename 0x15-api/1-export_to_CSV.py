783e72e2894e6b804c5d39632c066f5bf55fd64d
Sangwani <mightyp2@gmail.com>
2022-04-12 05:58:00 +0200

    fix PEP8 style

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     user_url = requests.get(url + "users/{}".format(user_id)).json()
     username = user_url.get("username")
     todos_url = requests.get(url + "todos", params={"userId": user_id}).json()
-    
+
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]) 
+            [user_id, username, t.get("completed"), t.get("title")])
             for t in todos_url]
